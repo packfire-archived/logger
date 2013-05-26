29bf942f49e6b51a34e91aa64212a760ffde1ad9
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-26 12:28:12 +0800

    implementation of logger

---
+++ src/Packfire/Logger/File.php
@@ -0,0 +1,100 @@
+<?php
+/**
+* Packfire Logger
+* By Sam-Mauris Yong
+*
+* Released open source under New BSD 3-Clause License.
+* Copyright (c) Sam-Mauris Yong <sam@mauris.sg>
+* All rights reserved.
+*/
+
+namespace Packfire\Logger;
+
+use Packfire\Logger\Format\Standard;
+use Psr\Log\AbstractLogger;
+use Packfire\Logger\Format\FormatInterface;
+
+/**
+ * File class
+ *
+ * Provide standard logging to a log file
+ *
+ * @author Sam-Mauris Yong / sam@mauris.sg
+ * @copyright Copyright (c) 2013 Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Packfire\Logger
+ * @since 1.0.0
+ */
+class File extends AbstractLogger {
+
+    /**
+     * Unlimited file size
+     */
+	const SIZE_UNLIMITED = -1;
+
+    /**
+     * Standard formatting
+     */
+    const FORMAT_STANDARD = null;
+
+    /**
+     * The log file to write to
+     * @var string
+     * @since 1.0.0
+     */
+	protected $file;
+
+    /**
+     * The format of the log output
+     * @var Packfire\Logger\Format\FormatInterface
+     * @since 1.0.0
+     */
+    protected $format;
+
+    /**
+     * The maximum size of a log file
+     * @var integer
+     * @since 1.0.0
+     */
+	protected $maxSize;
+
+	public function __construct(
+			$file,
+		    FormatInterface $format = self::FORMAT_STANDARD,
+		  	$maxFileSize = self::SIZE_UNLIMITED){
+        if(!$file){
+            throw new \InvalidArgumentException('File parameter is invalid in creating a file logger.');
+        }
+        if(!$format){
+            $format = new Standard();
+        }
+
+		$this->file = $file;
+        $this->format = $format;
+		$this->maxSize = $maxFileSize;
+	}
+
+	public function log($level, $message, array $context = array()){
+        $this->write($this->format->format($level, $message, $context) . "\n");
+	}
+
+	protected function write($text){
+		$file = $this->file;
+
+		if($this->maxSize !== self::SIZE_UNLIMITED){
+			$inc = 0;
+			$pathinfo = pathinfo($file);
+			$ext = isset($pathinfo['extension']) ? '.' . $pathinfo['extension'] : '';
+			while(file_exists($file) && filesize($file) > $this->maxSize){
+				++$inc;
+				$file = $pathinfo['dirname'] . '/' . $pathinfo['basename']
+				 . '-' . $inc . $ext;
+			}
+		}
+
+		$fh = fopen($file, 'a');
+		fwrite($fh, $text);
+		fclose($fh);
+	}
+
+}
---
+++ src/Packfire/Logger/Format/Standard.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Packfire Logger
+* By Sam-Mauris Yong
+*
+* Released open source under New BSD 3-Clause License.
+* Copyright (c) Sam-Mauris Yong <sam@mauris.sg>
+* All rights reserved.
+*/
+
+namespace Packfire\Logger\Format;
+
+/**
+ * Standard class
+ *
+ * The standard formatting for log entries
+ *
+ * @author Sam-Mauris Yong / sam@mauris.sg
+ * @copyright Copyright (c) 2013 Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Packfire\Logger\Format
+ * @since 1.0.0
+ */
+class Standard implements FormatInterface {
+
+    public function format($level, $message, array $context = array()){
+        return date('d M Y h:i.s O') . '[' . $level . '] ' 
+                . $message . ($context ? str_replace("\n", '', var_export($context, true)) : '');
+    }
+
+}
