d3c464490999d15b605021f7d9fe2e4c0fd25e78
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-03 16:52:47 +0800

    addressing standard format issue #2

--- src/Packfire/Logger/Format/Standard.php
+++ src/Packfire/Logger/Format/Standard.php
@@ -27,6 +27,6 @@ class Standard implements FormatInterface
     public function format($level, $message, array $context = array())
     {
         return date('d M Y h:i.s O') . ' [' . $level . '] '
-                . $message . ($context ? str_replace("\n", '', var_export($context, true)) : '');
+                . $message . ($context ? ' ' . str_replace("\n", '', var_export($context, true)) : '');
     }
 }
