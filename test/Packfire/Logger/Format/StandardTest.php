c59b25e36ac6dd9a3b2dd2c8e52da75bd9c5ecbf
Sam-Mauris Yong <mauris@hotmail.sg>
2014-01-04 09:19:51 +0800

    test implementation for standard format

--- test/Packfire/Logger/Format/StandardTest.php
+++ test/Packfire/Logger/Format/StandardTest.php
@@ -4,4 +4,10 @@ namespace Packfire\Logger\Format;
 
 class StandardTest extends \PHPUnit_Framework_TestCase
 {
+    public function testFormat()
+    {
+        $format = new Standard();
+        $result = $format->format('info', 'test message', array('test' => 1));
+        $this->assertRegExp('/^\d{2} \w{3} \d{4} \d{2}:\d{2}\.\d{2} \+\d{4} \[info\] "test message" array \(  \'test\' => 1,\)$/', $result);
+    }
 }
